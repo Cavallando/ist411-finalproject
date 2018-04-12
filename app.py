7b1cf92486e228d5c12e432d54d016006df3c7c7
Michael Cavallaro <michaelcavallaro03@gmail.com>
2018-04-12 18:02:06 -0400

    converting to a react app

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, flash, request, session, redirect, url_for
-app = Flask(__name__, static_folder='static', template_folder='templates')
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
