79d51c212c72c4a13c8029d333fb710a4367b262
Michael Cavallaro <michaelcavallaro03@gmail.com>
2018-05-01 19:17:09 -0400

    Auth deploy changes

--- src/utils/Auth/AuthConfig.js
+++ src/utils/Auth/AuthConfig.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'cavallaro.auth0.com',
   clientId: 'DAqjk8dCiDVT92kKaFePHydBq552Ae1l',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://paintify.herokuapp.com/callback'
 }
